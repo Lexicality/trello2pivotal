ba496a6eeb685bef526c095c6b9213cac6361a63
Lex Robinson <lex.robinson@smarkets.com>
2016-04-15 17:08:28 +0100

    Correctly doing things based on labels

--- index.js
+++ index.js
@@ -11,14 +11,58 @@ const config = toml.parse(fs.readFileSync('config.toml'));
 
 Q.longStackSupport = true;
 
+/*
+[ 'Needs to be done',
+  'Code Quality',
+  'Responsiveness',
+  'Nice to do',
+  'Good idea to do',
+  'Urgent to be done',
+  'Visuals',
+  'Bug',
+  'Usability issue',
+  'Icebox' ]
+*/
 
 function storyify(card) {
-    return {
+    let story = {
         name: card.name,
         description: card.desc,
         story_type: 'feature',
+        labels: [],
     };
+    for (let label of card.labels) {
+        switch (label.name) {
+        case 'Urgent to be done':
+        case 'Needs to be done':
+            story.current_state = 'unstarted';
+            break;
+        case 'Good idea to do':
+        case 'Nice to do':
+        case 'Icebox':
+            story.current_state = 'unscheduled';
+            break;
+        case 'Code Quality':
+            story.story_type = 'chore';
+            break;
+        case 'Bug':
+            story.story_type = 'bug';
+            break;
+        default:
+            story.labels.push({ name: label.name });
+        }
+    }
+    return story;
 }
+
+// function determineTrelloLabels(labels) {
+    // console.log(_.map(labels, 'name'));
+    // return {
+        // decorative: {},
+        // prios: {},
+    // };
+// }
+
 
 
 async function fn2() {
@@ -32,13 +76,19 @@ async function fn2() {
 
     await Q.all([ptLogin, ppLogin]);
 
-    let cards = trelloGet(`/1/boards/${config.trello.board}/cards/visible/`)
+    const boardURL = `/1/boards/${config.trello.board}`;
+
+    // await trelloGet(boardURL + '/labels').then(determineTrelloLabels);
+
+    let cards = trelloGet(`${boardURL}/cards/visible/`)
         .then((ret) => ret.filter((card) => _.includes(config.trello.lists, card.idList)));
 
     for (let card of await cards) {
+        // console.info(card);
         console.info('Transfering across card "%s"!', card.name);
         let story = await pivotalAPI.post(`/projects/${config.pivotal.project}/stories`, storyify(card));
         console.info('Created story %s!', story.id);
+        // break;
     }
 
     // console.info('Got #%d cards from trello!', cards.length);
